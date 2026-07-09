c4a5a21fce94854b1cda5e0e43331199180c267c
yousef alsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-07-09 21:56:49 +0300

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.js
@@ -0,0 +1,42 @@
+class NumMatrix {
+    /**
+     * @param {number[][]} matrix
+     */
+    constructor(matrix) {
+        this.matrix = matrix;
+		let n = matrix.length;
+		let m = matrix[0].length;
+        //to create 2d array filled with 0
+		this.prefix = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+		for (let i = 1; i <= n; i++) {
+			for (let j = 1; j <= m; j++) {
+				this.prefix[i][j] =
+					matrix[i - 1][j - 1] + 
+					this.prefix[i - 1][j] +  // above
+					this.prefix[i][j - 1] -  // left
+					this.prefix[i - 1][j - 1];  // top left
+			}
+		}
+    }
+
+    /**
+     * @param {number} row1
+     * @param {number} col1
+     * @param {number} row2
+     * @param {number} col2
+     * @return {number}
+     */
+    sumRegion(row1, col1, row2, col2) {
+        let result = (this.prefix[row2 + 1][col2 + 1] -  // current 
+				this.prefix[row1][col2 + 1] -    //above
+				this.prefix[row2 + 1][col1] +    //left
+				this.prefix[row1][col1])        // top left to add it 
+        return result
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
