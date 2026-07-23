95c9a2e3824f7ae75cf6145b722df48106dd37cb
yousef alsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-07-23 13:40:29 +0300

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+
+        let map= {
+            ')':'(',
+            '}':'{',
+            ']':'[',
+        }
+        if(s.length %2 != 0){
+            return false;
+        }
+
+        for(let i=0;i<s.length;i++){
+            if(s[i]==='[' || s[i]==='(' || s[i]==='{' ){
+                stack.push(s[i]);
+            }else{
+                if(stack.pop() !== map[s[i]]){
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+}
