602d288b6ccd32120bfdecd938bf06827c442d60
yousef alsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-07-25 17:12:40 +0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,42 @@
+
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        if(this.minStack.length ===  0 || val <= this.minStack[this.minStack.length - 1]){
+            this.minStack.push(val);
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        const val = this.stack.pop();
+        if(val === this.minStack[this.minStack.length - 1]){
+            this.minStack.pop()
+        }
+       }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1]; 
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1]; 
+    }
+}
