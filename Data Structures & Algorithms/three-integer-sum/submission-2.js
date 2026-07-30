17b678f68e5a729d27ca8d9084de7170ef4c99c3
yousef alsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-07-30 15:43:38 +0300

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b)=>a-b);
+        const res = [];
+        let ans = this.twoPts(nums,nums[0],1,nums.length-1);
+        if(ans){
+            res.push(...ans);
+        }
+        for(let i=1;i<nums.length;i++){
+            if(nums[i] !== nums[i-1]){
+                ans = this.twoPts(nums,nums[i],i+1,nums.length-1);
+                if(ans){
+                    res.push(...ans);
+                }
+                }
+        }
+        return res;
+        
+    }
+    twoPts(nums,target,l,r){
+        const res = [];
+        while(l<r){
+            const total = target + nums[l] + nums[r];
+            if(total === 0){
+                res.push([target,nums[l],nums[r]]);
+                l++;
+                r--;
+                while (l < r && nums[l] === nums[l - 1]) l++;
+            }else if(total<0){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        if(res.length)
+            return res;
+    }
+}
