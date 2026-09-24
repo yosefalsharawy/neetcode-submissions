5a3c8589b7cfe3b0177fefc498fffb7966434749
Youssef Elsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-09-24 18:10:55 +0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {};
+        let value;
+        for(let i=0;i<nums.length;i++){
+            value = target - nums[i];
+            if(map[value] !== undefined){
+                return [i,map[value]]
+            }
+            map[nums[i]] = i;
+        }
+    }
+}
