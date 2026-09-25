45cdf36c1e8e3021c372cbb5c7cb8bfd0c2ad5f5
Youssef Elsharawy <130904738+yosefalsharawy@users.noreply.github.com>
2026-09-25 15:32:19 +0300

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false;
+        }
+        const countS = {};
+        const countT = {};
+        for(let i=0;i<s.length;i++){
+            countS[s[i]] = (countS[s[i]] || 0) + 1;
+            countT[t[i]] = (countT[t[i]] || 0) + 1;
+        }
+        for(const [key,value] of Object.entries(countS)){
+            if(value !== countT[key]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
